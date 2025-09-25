b4cafff57f4d705661de5ae8086883ad70b93381
Harsh <harshva27@gmail.com>
2025-09-25 10:35:57 +0000

    Added wandb logging(hopefully correctly)

--- Model.py
+++ Model.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,6 +10,8 @@ from Attention import apply_rope
 from tqdm import tqdm
 from Dataloader import TextCorpusDataset, collate_fn
 from torch.utils.data import DataLoader, Dataset
+import wandb
+import argparse
 
 class Model(nn.Module):
     def __init__(self, num_layers = 4, embedding_size = 256, num_heads = 8, mlp_ratio = 4, bias = False, vocab_size = 1024):
@@ -30,21 +33,54 @@ class Model(nn.Module):
         return input_ids
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Model for training a very simple decoder only transformer")
+    parser.add_argument("--project_name",type=str, required=True,  help="Name of project(this will also be the saving directory")
+    parser.add_argument("--optimizer",type=str, required=True,  help="Which optimizer to use")
+    parser.add_argument("--learning_rate",type=float, required=True,  help="The learning rate to use")
+    parser.add_argument("--epochs", type=int, required=True, help="Total number of epochs")
+    parser.add_argument("--save_freq", type=int, default=10, help="Save frequency of model")
+    parser.add_argument("--block_size", type=int, default=256, help="Block size, basically till what token number the predictions will it be good")
+    parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
+    args = parser.parse_args()
+    wandb.login()
+    run = wandb.init(
+            project=args.project_name,
+            config={
+                'optimizer': args.optimizer,
+                "lr": args.learning_rate,
+                "epochs": args.epochs,
+                "save_freq": args.save_freq,
+                "block_size": args.block_size,
+                "batch_size": args.batch_size
+                })
     device = "cuda"
     model = Model().to(device)
     #x = "Hello my name is Anthony Gonzalves"
     #input_ids = model.tokenize(x)
     #output = model(input_ids)
-    epochs = 1000
-    block_size = 256
-    batch_size = 128
+    epochs = args.epochs
+    block_size = args.block_size
+    batch_size = args.batch_size
     tokenizer_path = '/workspace/Personal/Transformer/tokenizer_smaller.pkl'
     with open(tokenizer_path, 'rb') as f:
         tokenizer = pickle.load(f)
     dataset = TextCorpusDataset(txt_dir = "/workspace/Personal/Transformer/shakespeare-dataset/text", tokenizer = tokenizer, block_size = block_size)
     dataloader = DataLoader(dataset, batch_size = batch_size, shuffle = True, drop_last = True, collate_fn = collate_fn)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=0.01, weight_decay=0.01)
+    if(args.optimizer.lower() == "adamw"):
+        optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=args.learning_rate,            # better than 1e-3 for small model
+        betas=(0.9, 0.95),  # GPT-2 defaults
+        eps=1e-8    # turn off for Shakespeare
+        )
+    else:
+        raise ValueError(f"Not implemented {args.optimizer} yet")
+    Min_loss = 1e9
+    loss_list = []
+    BASE_PATH = 'runs/' + args.project_name
+    os.makedirs(BASE_PATH, exist_ok=True)
+
     for epoch in tqdm(range(epochs)):
         losses = 0.0
         for batch_idx, (inputs, targets) in enumerate(dataloader):
@@ -59,6 +95,12 @@ if __name__ == "__main__":
             #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             #print("Done for batch", batch_idx)
-        print("avg losses", losses/(batch_idx+1))
-        torch.save(model.state_dict(), f'model_epoch_{epoch}.pt')
-
+        loss_list += [losses]
+        wandb.log({"loss":losses})
+        print(sum(loss_list)/len(loss_list))
+        if(epoch%args.save_freq == 0):
+            torch.save(model.state_dict(), f'{BASE_PATH}/model_epoch_{epoch}.pt')
+        if(losses<Min_loss):
+            torch.save(model.state_dict(), f'{BASE_PATH}/best_model.pt')
+            Min_loss = losses
+    run.finish()
